98804dbeb82690f4c6d0b4cc17cec3cabd6880c8
GaryChern <2323989781@qq.com>
2021-12-08 15:48:47 +0800

    urls modified(20211208-15:48)

--- Login/urls.py
+++ Login/urls.py
@@ -5,4 +5,5 @@ import views
 
 urlpatterns = [
     url(r'^$',views.login_view),#创建一个新的应用
+    url(r'^login01/',views.login01_view),
 ]
--- Login/views.py
+++ Login/views.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def login_view(request):
     return render(request,'Login.html')
+
+
+def login01_view(request):
+    #接受表单请求参数
+    uname=request.GET.get('uname','')
+    pwd=request.GET.get('pwd','')
+    #get()相当于一个字典，其中k指的是表单中name，value表示输入框中输入的值
+
+    #判断
+    if uname=='zhangsan' and pwd=='123':
+          return HttpResponse('登陆成功！')
+
+    return HttpResponse('登陆失败！')
